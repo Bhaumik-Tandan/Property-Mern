f67104bfad65d2daf03be296c70aba4f6cb4a671
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-17 03:32:28 +0530

    deploy

--- src/restCon.js
+++ src/restCon.js
@@ -1,7 +1,7 @@
 export function restCon(payload,method,route)
 {
     payload=JSON.stringify(payload);
-    return fetch(route, {
+    return fetch("https://property-bhaumik-tandan.herokuapp.com/"+route, {
          method: method,
          body: payload,
          headers:{"Content-Type":"application/json"}
