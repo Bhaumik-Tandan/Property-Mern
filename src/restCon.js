8132cf7280e6f7b59e15d015685c925185baa5a2
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-17 03:05:44 +0530

    connection set

--- src/restCon.js
+++ src/restCon.js
@@ -1,7 +1,7 @@
 export function restCon(payload,method,route)
 {
     payload=JSON.stringify(payload);
-    return fetch("http://localhost:5000/"+route, {
+    return fetch(route, {
          method: method,
          body: payload,
          headers:{"Content-Type":"application/json"}
